08861dc201819275e93ced2abdbf8f0c973d8b73
HTBadmin <htbadmin@protonmail.ch>
2019-04-18 21:55:51 -0400

    Connect to last hop with napalm and start parsing routes

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-
-'''
-Assumptions:
-
-
-'''
-
 import os
 import re
 import sys
 import socket
 import argparse
 import subprocess
+
 from getpass import getpass
+from napalm import get_network_driver
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -28,7 +23,17 @@ def get_args():
             setattr(args, key, newvalue)
     return args
 
+def init_device(address, username):
+    driver = get_network_driver('ios')
+    password = getpass('Enter SSH Password: ')
+    device = driver(address, username, password)
+    print('Connecting to ' + address + '...')
+    device.open()
+    return device
+
+
 def trace(addr, max_hops):
+    print("Tracing path to " + addr)
     command = "tracepath -m " + str(max_hops) + " " + addr
     path = subprocess.check_output(command, shell=True)
     return path
@@ -45,17 +50,29 @@ def parse_path(path, target_ip):
     return new_path
 
 def main():
+
     args = get_args()
     target_ip = args.target_ip
     username = args.username
     max_hops = args.max_hops
-    #password = getpass('Enter SSH Password: ')
+
     path = trace(target_ip, max_hops)
     path = parse_path(path, target_ip)
     last_hop = path[1]
     ip = re.compile('((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
     test = ip.search(last_hop)
-    print(test.group())
+    last_hop = test.group()
+    print("Last address in path: " + last_hop)
+    connect = input("Connect to " + last_hop + "? (Y/n): ")
+    if "yes" in connect.lower() or "y" in connect.lower():
+        device = init_device(last_hop, username)
+        command = "show ip route {}".format(target_ip)
+        out = device._send_command(command)
+        for line in out.split('\n'):
+            if '*' in line:
+                print(line)
+    else:
+        print("Bye...")
 
 if __name__ == '__main__':
     main()
